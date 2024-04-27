8002586cb4f7b405b4b8d41cb8d4f41be16eabe4
Developer294 <juandelestal1994@gmail.com>
2024-04-27 14:50:10 -0300

    media-queries ok

--- client/index.js
+++ client/index.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Agregar clases después de que la página se haya cargado completamente
-  window.addEventListener('load', function() {
     setTimeout(function() {
       document.getElementById('nombre-juan').classList.add('loaded');
       document.getElementById('sub-juan').classList.add('loaded');
       document.getElementById('foto-juan').classList.add('foto-con-borde');
       document.getElementById('navbar').classList.add('nav-loaded');
     }, 100); // Ajusta el tiempo según sea necesario
-  });
 });
