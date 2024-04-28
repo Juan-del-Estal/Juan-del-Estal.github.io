4a086c679e36685694c25a24ef2b9e39ff93b43d
Developer294 <juandelestal1994@gmail.com>
2024-04-28 20:23:48 -0300

    media-queries ok

--- client/index.js
+++ client/index.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Agregar clases después de que la página se haya cargado completamente
-    setTimeout(function() {
+    window.addEventListener('scroll', () => {
+      let navBar = document.getElementById('navbar');
+      let allLinks = document.querySelectorAll('a');
+      navBar.classList.toggle("color-nav", window.scrollY > 0)
+      
+      // Loop through each <a> element and toggle the class
+      allLinks.forEach(link => {
+        link.classList.toggle("links-nav", window.scrollY > 0);
+      });
+    })
+    setTimeout( () => {
       document.getElementById('nombre-juan').classList.add('loaded');
       document.getElementById('sub-juan').classList.add('loaded');
       document.getElementById('foto-juan').classList.add('foto-con-borde');
