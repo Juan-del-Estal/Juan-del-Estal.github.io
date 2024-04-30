3b50a12f0e5633c92acb23ab1572fd277b2e2152
Developer294 <juandelestal1994@gmail.com>
2024-04-30 11:11:14 -0300

    media-queries ok

--- client/index.js
+++ client/index.js
@@ -1,38 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Check if the device is a mobile device
-  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
   // Smooth scrolling when clicking on links with class "scroll-link"
   $('.scroll-link').on('click', function (event) {
-      if (!isMobile && this.hash !== '') { // Check if not a mobile device
-          event.preventDefault();
+   if (this.hash !== '') {
+     event.preventDefault();
 
-          var hash = this.hash;
-          // Animate scroll to target
-          $('html, body').animate({
-              scrollTop: $(hash).offset().top
-          }, 600, function () {
-              // Add hash (#) to URL when done scrolling (default click behavior)
-              window.location.hash = hash;
-          });
-      }
-  });
-  
-  window.addEventListener('scroll', () => {
-      let navBar = document.getElementById('navbar');
-      let allLinks = document.querySelectorAll('a');
-      navBar.classList.toggle("color-nav", window.scrollY > 0)
-    
-      // Loop through each <a> element and toggle the class
-      allLinks.forEach(link => {
-          link.classList.toggle("links-nav", window.scrollY > 0);
-      });
-  });
-
-  setTimeout( () => {
-      document.getElementById('nombre-juan').classList.add('loaded');
-      document.getElementById('sub-juan').classList.add('loaded');
-      document.getElementById('foto-juan').classList.add('foto-con-borde');
-      document.getElementById('navbar').classList.add('nav-loaded');
-  }, 100); // Adjust timing as necessary
+     var hash = this.hash;
+     // Animate scroll to target
+     $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 600, function () {
+       // Add hash (#) to URL when done scrolling (default click behavior)
+       window.location.hash = hash;
+     });
+   }
+ });
+ 
+ window.addEventListener('scroll', () => {
+   let navBar = document.getElementById('navbar');
+   let allLinks = document.querySelectorAll('a');
+   navBar.classList.toggle("color-nav", window.scrollY > 0)
+   
+   // Loop through each <a> element and toggle the class
+   allLinks.forEach(link => {
+     link.classList.toggle("links-nav", window.scrollY > 0);
+   });
+ })
+ setTimeout( () => {
+   document.getElementById('nombre-juan').classList.add('loaded');
+   document.getElementById('sub-juan').classList.add('loaded');
+   document.getElementById('foto-juan').classList.add('foto-con-borde');
+   document.getElementById('navbar').classList.add('nav-loaded');
+ }, 100); // Ajusta el tiempo según sea necesario
 });
