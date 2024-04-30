1f909c9c7185d76492c676fa5fc9c9b746637387
Developer294 <juandelestal1994@gmail.com>
2024-04-30 16:13:22 -0300

    media-queries ok

--- client/index.js
+++ client/index.js
@@ -1,17 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let allLinks = document.querySelectorAll('a');
-  
+  let allLinks = document.querySelectorAll('.link-nav');
+  let sectionClicked;
+
   allLinks.forEach(link => {
     link.addEventListener('click', () => {
+      if(sectionClicked){
+        sectionClicked.classList.remove('selected-section')
+        sectionClicked.classList.remove('transition-section')
+      }
       // Extract "#" from the href´s
       let sectionIdWithNumeral = link.getAttribute('href')
       let sectionId = sectionIdWithNumeral.replace('#', '');
       
-      let section = document.getElementById(sectionId);
-      section.classList.add('selected-section');
-      section.classList.add('transition-section');
+      sectionClicked = document.getElementById(sectionId);
+      sectionClicked.classList.add('selected-section');
+      sectionClicked.classList.add('transition-section');
+    });
   });
-  
+
   window.addEventListener('scroll', () => {
    let navBar = document.getElementById('navbar');
    let allLinks = document.querySelectorAll('a');
@@ -21,12 +27,11 @@ document.addEventListener("DOMContentLoaded", function() {
    allLinks.forEach(link => {
      link.classList.toggle("links-nav", window.scrollY > 0);
    });
- })
- setTimeout( () => {
-   document.getElementById('nombre-juan').classList.add('loaded');
-   document.getElementById('sub-juan').classList.add('loaded');
-   document.getElementById('foto-juan').classList.add('foto-con-borde');
-   document.getElementById('navbar').classList.add('nav-loaded');
- }, 100); // Ajusta el tiempo según sea necesario
-});
+  })
+  setTimeout( () => {
+    document.getElementById('nombre-juan').classList.add('loaded');
+    document.getElementById('sub-juan').classList.add('loaded');
+    document.getElementById('foto-juan').classList.add('foto-con-borde');
+    document.getElementById('navbar').classList.add('nav-loaded');
+  }, 100); // Ajusta el tiempo según sea necesario
 });
