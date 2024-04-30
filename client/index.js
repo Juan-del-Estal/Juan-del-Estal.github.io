05a624a5d7b25c45afefc4cfec6419dc178d3a1a
Developer294 <juandelestal1994@gmail.com>
2024-04-30 14:24:14 -0300

    media-queries ok

--- client/index.js
+++ client/index.js
@@ -1,24 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Smooth scrolling when clicking on links with class "scroll-link"
-  $('.scroll-link').on('click', function (event) {
-   if (this.hash !== '') {
-     event.preventDefault();
-
-     var hash = this.hash;
-     // Animate scroll to target
-     $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, 600, function () {
-       // Add hash (#) to URL when done scrolling (default click behavior)
-       window.location.hash = hash;
-     });
-   }
- });
- 
- window.addEventListener('scroll', () => {
+  let allLinks = document.querySelectorAll('a');
+  
+  allLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      // Extract "#" from the href´s
+      let sectionIdWithNumeral = link.getAttribute('href')
+      let sectionId = sectionIdWithNumeral.replace('#', '');
+      
+      let section = document.getElementById(sectionId);
+      section.classList.add('selected-section');
+      section.classList.add('transition-section');
+  });
+  
+  window.addEventListener('scroll', () => {
    let navBar = document.getElementById('navbar');
    let allLinks = document.querySelectorAll('a');
-   navBar.classList.toggle("color-nav", window.scrollY > 0)
+   navBar.classList.toggle("color-nav", window.scrollY > 0);
    
    // Loop through each <a> element and toggle the class
    allLinks.forEach(link => {
@@ -31,4 +28,5 @@ document.addEventListener("DOMContentLoaded", function() {
    document.getElementById('foto-juan').classList.add('foto-con-borde');
    document.getElementById('navbar').classList.add('nav-loaded');
  }, 100); // Ajusta el tiempo según sea necesario
+});
 });
